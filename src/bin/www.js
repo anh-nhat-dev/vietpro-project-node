8e19d62f64e6998eef4ed40d9b6e3b0b7d373984
Cao Anh Nhất <anhnhata8cool@gmail.com>
2020-11-08 21:32:01 +0700

    socket.io

--- src/bin/www.js
+++ src/bin/www.js
@@ -2,11 +2,20 @@
 
 const app = require("../apps/app");
 const config = require("config");
+const socket = require("socket.io");
+const { isValidObjectId } = require("mongoose");
 
 // Lấy port từ trong config
 const port = config.get("app.port");
 
 // Lắng nghe các yêu cầu trên port
+
 const server = app.listen(port, function () {
   console.info(`Server listening on port ${port}`);
+});
+
+const io = socket(server);
+
+io.on("connection", function (socket) {
+  console.log("Client connect");
 });
